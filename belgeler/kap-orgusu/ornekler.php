bdfd831b84a05505b7ce600299f2d6b8350a904e
Django Öğreniyorum <djangoogreniyorum@gmail.com>
2017-08-15 20:10:12 +0300

    Bazı hatalar düzeltildi.

--- belgeler/kap-orgusu/ornekler.php
+++ belgeler/kap-orgusu/ornekler.php
@@ -1,7 +1,7 @@
 <?php include '../../toprak/head.html';?>
 <div class="kap-merkez">
   <div class="kap_gnl-11-12p">
-    <div class="kap_gnl-3b arala-1 dışkenar-0">
+    <div class="kap_gnl-3b_ort-1b arala-1 dışkenar-0">
       <div class="kap_gnl-5-5p">
         <h1>Kap Örgüsü Örnekleri</h1>
         <p>Parça, bölme, oran yöntemiyle oluşturulan kap örgüleri örneklerini buradan inceleyebilirsiniz. Aynı zamanda bu örnekler genellikle ihtiyaç duyulan kap örgüleri olduğu için tasarımlarınızda kullanmak için buradan kopyalayabilirsiniz. Kap örgüsü IE'nin eski sürümlerinde çalışmayabilir. Diğer internet tarayıcılarında uyum sorunu bulunmamaktadır.</p>
@@ -28,13 +28,13 @@
       </div>
       <div class="kap_gnl-0p">
         <h2>Parça</h2>
-        <p>İçinde bulunan genişliği belirtilen parça miktarına bölerek yazılan genişlik yöntemidir. 5p 6p 8p 9p 10p 12p 16p 18p 20p 24p olmak üzere 10 farklı parça düzenini aynı anda kullanabilirsiniz.</p>
+        <p>İçinde bulunan genişliği belirtilen parça miktarına bölerek yazılan genişlik yöntemidir. 5p 6p 8p 9p 10p 12p 16p 18p 20p 24p düzenini aynı anda kullanabilirsiniz.</p>
         <iframe width="100%" height="300" src="//jsfiddle.net/deveb/ke420f56/embedded/result,html/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
       </div>
       <div class="kap_gnl-0p">
-        <h2>Eşit Bölme</h2>
-        <p>Bir genişliğin içine yazılmış "-0p" değerini alan her sınıfı eşit bölmelere ayırır.</p>
-        <iframe width="100%" height="300" src="//jsfiddle.net/deveb/rc94Lwk1/embedded/result,html/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
+        <h2>Parça ve Eşit Bölme</h2>
+        <p>Bir genişliği istenilen genişlikteki parçalara bölüp içine eklenen kapları eşit oranda bölme yapmak.</p>
+        <iframe width="100%" height="300" src="//jsfiddle.net/deveb/nbetvefq/1/embedded/result,html/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
       </div>
       <div class="kap_gnl-0p">
         <h2>Eşit Bölme</h2>
