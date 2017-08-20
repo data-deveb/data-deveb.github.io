0db95237baab990e76e02207848d0516bacad3ed
Django Öğreniyorum <djangoogreniyorum@gmail.com>
2017-08-20 14:30:54 +0300

    Güncellemeler

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'toprak/head.html';?>
 <?php include 'toprak/header.html';?>
-<div class="kap_5-5p-merkez">
-    <div class="kap_gnl-4-8p_kçk-8-8p-orta">
+<div class="kap gnl-5-5p ortala">
+    <div class="kap gnl-4-8p kçk-8-8p ortala">
       <div class="dolgu-2" style="overflow:hidden;">
         <h1 class="metin-deveb  beri-2" style="font-size:80pt;">deveb</h1>
         <p class="metin-gri-3 beri-2">
@@ -10,28 +10,28 @@ Kap örgüsünün sahip olduğu özellikleri bir kere kullandıktan sonra ne kad
         </p>
       </div>
     </div>
-    <div class="kap_gnl-4-8p_kçk-8-8p-merkez">
-      <div class="kap_gnl-6-8p">
-        <div class="kap_gnl-5-5p beri-alt-1" style="background-color:#ccc; height:200px;">
+    <div class="kap gnl-4-8p kçk-8-8p-merkez">
+      <div class="kap gnl-6-8p">
+        <div class="kap gnl-5-5p beri-alt-1" style="background-color:#ccc; height:200px;">
 
         </div>
-        <div class="kap-gnl-5-5p ap-deveb beri-alt-1" style="height:60px;">
+        <div class="kap gnl-5-5p ap-deveb beri-alt-1" style="height:60px;">
 
         </div>
-        <div class="kap-gnl-1-5p ap-deveb beri-alt-1" style="height:80px;">
+        <div class="kap gnl-1-5p ap-deveb beri-alt-1" style="height:80px;">
 
         </div>
-        <div class="kap-gnl-4-5p_gnl-eb">
-          <div class="kap_gnl-0p beri-sol-1 beri-alt-1" style="background-color:#ccc;">
+        <div class="kap gnl-4-5p gnl-eb">
+          <div class="kap gnl-0p beri-sol-1 beri-alt-1" style="background-color:#ccc;">
 
           </div>
         </div>
-        <div class="kap-gnl-5-5p ap-deveb" style="height:60px;">
+        <div class="kap gnl-5-5p ap-deveb" style="height:60px;">
 
         </div>
       </div>
-      <div class="kap_gnl-2-8p_gnl-eb">
-        <div class="kap_gnl-0p beri-sol-2 ap-deveb">
+      <div class="kap gnl-2-8p gnl-eb">
+        <div class="kap gnl-0p beri-sol-2 ap-deveb">
 
         </div>
       </div>
