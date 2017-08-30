390dc2628116b5d2dda7c134881c48c0478cff23
Django Öğreniyorum <djangoogreniyorum@gmail.com>
2017-08-31 01:53:32 +0300

    Genişliklerde tespit edilen hatalı değer düzeltildi.

---
+++ belgeler/menuler.php
@@ -0,0 +1,11 @@
+<?php include '../toprak/head.html';?>
+<?php include '../toprak/header.html';?>
+<div class="ortala" gnl="24p">
+  <div class="" gnl="23">
+    Menüler
+
+  </div>
+</div>
+
+
+<?php include '../toprak/foot.html';?>
