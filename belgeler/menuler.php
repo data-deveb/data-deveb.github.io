1377f23922db4600135748bb0fdb94f2088c3c0a
Django Öğreniyorum <djangoogreniyorum@gmail.com>
2017-08-31 20:52:17 +0300

    Kap örgüsüne ufak eklentiler.

--- belgeler/menuler.php
+++ belgeler/menuler.php
@@ -2,7 +2,88 @@
 <?php include '../toprak/header.html';?>
 <div class="ortala" gnl="24p">
   <div class="" gnl="23">
-    Menüler
+    <h1>Menüler</h1>
+    <ul>
+      <li>Seviye 1.1</li>
+      <li>Seviye 1.2
+        <ul>
+          <li>Seviye 2.1
+            <ul>
+              <li>Seviye 3.1</li>
+              <li>Seviye 3.2</li>
+              <li>Seviye 3.3
+                <ul>
+                  <li>Seviye 4.1</li>
+                  <li>Seviye 4.2</li>
+                  <li>Seviye 4.3</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>Seviye 2.2</li>
+          <li>Seviye 2.3</li>
+        </ul>
+      </li>
+      <li>Seviye 1.3</li>
+    </ul>
+
+    <h1>Simple Pure CSS Drop Down Menu</h1>
+    <nav id="primary_nav_wrap">
+    <ul>
+      <li class="current-menu-item"><a href="#">Home</a></li>
+      <li><a href="#">Menu 1</a>
+        <ul>
+          <li><a href="#">Sub Menu 1</a></li>
+          <li><a href="#">Sub Menu 2</a></li>
+          <li><a href="#">Sub Menu 3</a></li>
+          <li><a href="#">Sub Menu 4</a>
+            <ul>
+              <li><a href="#">Deep Menu 1</a>
+                <ul>
+                  <li><a href="#">Sub Deep 1</a></li>
+                  <li><a href="#">Sub Deep 2</a></li>
+                  <li><a href="#">Sub Deep 3</a></li>
+                    <li><a href="#">Sub Deep 4</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Deep Menu 2</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Sub Menu 5</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Menu 2</a>
+        <ul>
+          <li><a href="#">Sub Menu 1</a></li>
+          <li><a href="#">Sub Menu 2</a></li>
+          <li><a href="#">Sub Menu 3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Menu 3</a>
+        <ul>
+          <li class="dir"><a href="#">Sub Menu 1</a></li>
+          <li class="dir"><a href="#">Sub Menu 2 THIS IS SO LONG IT MIGHT CAUSE AN ISSEUE BUT MAYBE NOT?</a>
+            <ul>
+              <li><a href="#">Category 1</a></li>
+              <li><a href="#">Category 2</a></li>
+              <li><a href="#">Category 3</a></li>
+              <li><a href="#">Category 4</a></li>
+              <li><a href="#">Category 5</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Sub Menu 3</a></li>
+          <li><a href="#">Sub Menu 4</a></li>
+          <li><a href="#">Sub Menu 5</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Menu 4</a></li>
+      <li><a href="#">Menu 5</a></li>
+      <li><a href="#">Menu 6</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+    </nav>
+
+
 
   </div>
 </div>
