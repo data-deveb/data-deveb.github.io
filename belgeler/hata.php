b0ab67ee012b4159781eaa87973d55a0ab04446d
Django Öğreniyorum <djangoogreniyorum@gmail.com>
2017-09-05 03:07:42 +0300

    Kolon örgüsü eklendi.

--- belgeler/hata.php
+++ belgeler/hata.php
@@ -1,35 +1,29 @@
 <?php include '../toprak/head.html';?>
-<div class="ortala" gnl="24p">
+<div class="ortala" gnl="3 kolon 2p">
 
-
-
-<div class="yan-sekmeler" gnl="23">
-  <input type="radio" name="ys" id="t3ss4a" checked>
-  <input type="radio" name="ys" id="c1veza">
-  <input type="radio" name="ys" id="er2fda">
-  <input type="radio" name="ys" id="yud34D">
-  <div class="sekme" gnl="4">
-    <label for="t3ss4a"><!-- Sekme adını buraya girin --></label>
-    <label for="c1veza"><!-- Sekme adını buraya girin --></label>
-    <label for="er2fda"><!-- Sekme adını buraya girin --></label>
-    <label for="yud34D"><!-- Sekme adını buraya girin --></label>
-  </div>
-  <div class="icerik" gnl="20">
-    <div class="" gnl="24">
-      <h1>Sekme #1</h1>
-      <p>Lorem Ipsum,</p>
-    </div>
-    <div class="" gnl="24">
-      <!-- Sekme içeriğini buraya girin -->
-    </div>
-    <div class="" gnl="24">
-      <!-- Sekme içeriğini buraya girin -->
-    </div>
-    <div class="" gnl="24">
-      <!-- Sekme içeriğini buraya girin -->
-    </div>
-  </div>
-</div>
-
+  <div gnl="1">1/2p #1</div>
+  <div gnl="1">1/2p #2</div>
+  <div gnl="1">1/2p #3</div>
+  <div gnl="1">1/2p #4</div>
+  <div gnl="1">1/2p #5</div>
+  <div gnl="1">1/2p #6</div>
+  <div gnl="1">1/2p #7</div>
+  <div gnl="1">1/2p #8</div>
+  <div gnl="1">1/2p #9</div>
+  <div gnl="1">1/2p #10</div>
+  <div gnl="1">1/2p #11</div>
+  <div gnl="1">1/2p #12</div>
+  <div gnl="1">1/2p #13</div>
+  <div gnl="1">1/2p #14</div>
+  <div gnl="1">1/2p #15</div>
+  <div gnl="1">1/2p #16</div>
+  <div gnl="1">1/2p #17</div>
+  <div gnl="1">1/2p #18</div>
+  <div gnl="1">1/2p #19</div>
+  <div gnl="1">1/2p #20</div>
+  <div gnl="1">1/2p #21</div>
+  <div gnl="1">1/2p #22</div>
+  <div gnl="1">1/2p #23</div>
+  <div gnl="1">1/2p #24</div>
 </div>
 <?php include '../toprak/foot.html';?>
