ff3e460403aa0ce971d2d72ce4ba9f55040c311b
Django Öğreniyorum <djangoogreniyorum@gmail.com>
2017-09-16 23:49:48 +0300

    Yepyeni yapılanma

--- belgeler/butonlar.php
+++ belgeler/butonlar.php
@@ -1,21 +1,21 @@
 <?php include '../toprak/head.html';?>
 <?php include '../toprak/header.html';?>
-<div class="kaplama ortala" data-gnl="1p" data-ufk="3p">
-  <div data-gnl="1" data-ufk="1">
+<div class="kaplama ortala" data-deveb~="1p" data-deveb~="ufk-3p">
+  <div data-deveb~="1" data-deveb~="ufk-1">
     <div class="kutu kutu-genel beri-1 metin-orta arka-birincil arka-800 kenar-alt-tehlike metin-ana">
       <h2>Genel Buton</h2>
       <p class="metin-gri-3 ">Sıradan amaçlarla kullanılmak için</p>
       <button class="btn btn-genel beri-1" type="button" name="button">Buton</button>
     </div>
   </div>
-  <div data-gnl="1" data-ufk="1">
+  <div data-deveb~="1" data-deveb~="ufk-1">
     <div class="kutu kutu-genel beri-1 metin-orta" kenar="tam tehlike 300 soluk" arka="ara 300 soluk" metin="birincil 400">
       <h2>İmgeli Genel Buton</h2>
       <p class="metin-gri-3 ">Sıradan amaçlarla kullanılmak için</p>
       <button class="btn btn-genel beri-1" type="button" name="button"><i class="fa fa-asterisk" aria-hidden="true"></i> Buton</button>
     </div>
   </div>
-  <div data-gnl="1" data-ufk="1">
+  <div data-deveb~="1" data-deveb~="ufk-1">
     <div class="kutu kutu-genel beri-1 metin-orta">
       <h2>Sağda İmgeli Genel Buton</h2>
       <p class="metin-gri-3 ">Sıradan amaçlarla kullanılmak için</p>
